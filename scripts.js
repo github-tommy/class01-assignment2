06b3455b049254417b5b85b0836c5d2b6f0c4814
tom <tommy.surjadi@gmail.com>
2019-04-07 09:35:36 -0700

    Added more calculator

--- scripts.js
+++ scripts.js
@@ -2,9 +2,9 @@
 $(function () {
     // Set up some data and variables
     const data = [1, 2, 3, 4],
-        input = $('.input'),
-        submit = $('.submit'),
-        resultSum = $('.sum');
+        input = $('#input'),
+        submit = $('#submit'),
+        resultSum = $('#sum');
     // Click event on the submit input
     submit.on('click', function () {
         // Grab the input value and parse it into a number
@@ -24,27 +24,86 @@ $(function () {
     });
 });
 
-// ======ADDING THE NUMBER INPUTTED BY THE USER, BY 10
+// ======ADDING THE NUMBER INPUTTED BY THE USER ================================
 
-$(function() {
+// jQuery Doc Ready
+$(function () {
+    // Set up some data and variables
     const data = [1, 2, 3, 4],
-        input = $('.input'),
-        submit = $('.submit'),
-        resultMap = $('.map');
-    submit.on('click', function() {
+        input = $('#input2'),
+        submit = $('#submit2'),
+        resultSum = $('#sum2');
+    // Click event on the submit input
+    submit.on('click', function () {
+        // Grab the input value and parse it into a number
         const val = input.val();
         const num = parseInt(val);
-
+        // Verify the user gave us a real number
         if (!isNaN(num)) {
+            // Add our new number to the data array
+            // data.push(num);
+            // Use reduce to convert our array of numbers into a single value - the sum of all of the numbers
+            const sum = data.map((x) => x + num);
+            // Add our new total to the page
+            resultSum.text(sum);
+            // Reset the input value to an empty string
+            input.val('');
+        }
+    });
+});
 
+// ==========FILTER NUMBER <4========
+
+// jQuery Doc Ready
+$(function () {
+    // Set up some data and variables
+    const data = [1, 2, 3, 4],
+        input = $('#input3'),
+        submit = $('#submit3'),
+        resultSum = $('#sum3');
+    // Click event on the submit input
+    submit.on('click', function () {
+        // Grab the input value and parse it into a number
+        const val = input.val();
+        const num = parseInt(val);
+        // Verify the user gave us a real number
+        if (!isNaN(num)) {
+            // Add our new number to the data array
             data.push(num);
-
-            const map = data.map(function(x) {
-                return x + 10;
-            
-                resultMap.text(map);
-
-                input.val('');
-            });
+            // Use reduce to convert our array of numbers into a single value - the sum of all of the numbers
+            const sum = data.filter((x) => x > 3);
+            // Add our new total to the page
+            resultSum.text(sum);
+            // Reset the input value to an empty string
+            input.val('');
+        }
     });
+});
+
+// =============SHOW ALL CURRENT NUMBERS ====================
+
+// jQuery Doc Ready
+$(function () {
+    // Set up some data and variables
+    const data = [1, 2, 3, 4],
+        input = $('#input4'),
+        submit = $('#submit4'),
+        resultSum = $('#sum4');
+    // Click event on the submit input
+    submit.on('click', function () {
+        // Grab the input value and parse it into a number
+        const val = input.val();
+        const num = parseInt(val);
+        // Verify the user gave us a real number
+        if (!isNaN(num)) {
+            // Add our new number to the data array
+            data.push(num);
+            // Use reduce to convert our array of numbers into a single value - the sum of all of the numbers
+            const sum = data.forEach((x) => x);
+            // Add our new total to the page
+            resultSum.text(sum);
+            // Reset the input value to an empty string
+            input.val('');
+        }
     });
+});
