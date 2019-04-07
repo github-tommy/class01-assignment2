1d3d322fc44e63f6c542e2134bb331f2be16d7f3
tom <tommy.surjadi@gmail.com>
2019-04-07 12:35:40 -0700

    corrected .forEach

--- scripts.js
+++ scripts.js
@@ -80,29 +80,28 @@ $(function () {
     });
 });
 
-// =============SHOW ALL CURRENT NUMBERS ====================
+// =============TO SHOW ALL NUMBERS INCLUDING NEW# INPUTTED ====================
 
-// jQuery Doc Ready
 $(function () {
-    // Set up some data and variables
-    const data = [1, 2, 3, 4],
+
+    let data = [1, 2, 3, 4],
         input = $('#input4'),
         submit = $('#submit4'),
         resultSum = $('#sum4');
-    // Click event on the submit input
+
     submit.on('click', function () {
-        // Grab the input value and parse it into a number
-        const val = input.val();
-        const num = parseInt(val);
-        // Verify the user gave us a real number
+
+        let val = input.val();
+        let num = parseInt(val);
+
         if (!isNaN(num)) {
-            // Add our new number to the data array
+
             data.push(num);
-            // Use reduce to convert our array of numbers into a single value - the sum of all of the numbers
-            const sum = data.forEach((x) => x);
-            // Add our new total to the page
-            resultSum.text(sum);
-            // Reset the input value to an empty string
+
+            data.forEach(function(value) {
+                resultSum.text(data)
+            });
+
             input.val('');
         }
     });
